9986559a9914f7c12d33d677a7ca0579afea50d2
Clara Raubertas <clara@clararaubertas.net>
2011-09-30 14:20:22 -0400

    Revert "timezone finalize"
    This reverts commit c10b9ca9f22919770ebf228a6aa528cc454b7c96.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,14 +14,15 @@ $(document).ready(function(){
   });
 });
 
-include('detect-timezone.js');
-include('jquery-cookie.js');
+document.write("<script type='text/javascript' src='/javascripts/detect-timezone.js' ></script>");
+document.write("<script type='text/javascript' src='/javascripts/jquery-cookie.js' ></script>");
 
 
 
-<!-- Set timezone cookie -->
-if(!($.cookie('timezone'))) {
-   current_time = new Date();
-   $.cookie('timezone', jstz.determine_timezone().name(), { path: '/', expires: 10 } );
-}
+document.write("<!-- Set timezone cookie -->");
+document.write("<script type='text/javascript'>");
+document.write("if(!($.cookie('timezone'))) {");
+document.write("current_time = new Date();");
+document.write("$.cookie('timezone', jstz.determine_timezone().name(), { path: '/', expires: 10 } );};");
+document.write("</script>");
 
